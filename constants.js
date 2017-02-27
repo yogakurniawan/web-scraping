8cc74fb4cdfe040ec57eadd1497302de9ba06e6d
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-02-27 07:06:16 +0700

    boooooooom, roasted !!!

---
+++ constants.js
@@ -0,0 +1,256 @@
+var urls = {
+  SAMSUNG: {
+    FIRST: "samsung-phones-9.php",
+    NEXT: "samsung-phones-f-9-0-p{0}.php",
+    TOTAL_PAGE: 13,
+    BRAND_ID: "58797ed4312455ba35347b55",
+    KEYWORD: "samsung"
+  },
+  APPLE: {
+    FIRST: "apple-phones-48.php",
+    NEXT: "apple-phones-f-48-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b56",
+    KEYWORD: "apple"
+  },
+  MICROSOFT: {
+    FIRST: "microsoft-phones-64.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b57",
+    KEYWORD: "microsoft"
+  },
+  NOKIA: {
+    FIRST: "nokia-phones-1.php",
+    NEXT: "nokia-phones-f-1-0-p{0}.php",
+    TOTAL_PAGE: 7,
+    BRAND_ID: "58797ed4312455ba35347b58",
+    KEYWORD: "nokia"
+  },
+  SONY: {
+    FIRST: "sony-phones-7.php",
+    NEXT: "sony-phones-f-7-0-p{0}.php",
+    TOTAL_PAGE: 4,
+    BRAND_ID: "58797ed4312455ba35347b59",
+    KEYWORD: "sony"
+  },
+  LG: {
+    FIRST: "lg-phones-20.php",
+    NEXT: "lg-phones-f-20-0-p{0}.php",
+    TOTAL_PAGE: 10,
+    BRAND_ID: "58797ed4312455ba35347b5a",
+    KEYWORD: "lg"
+  },
+  HTC: {
+    FIRST: "htc-phones-45.php",
+    NEXT: "htc-phones-f-45-0-p{0}.php",
+    TOTAL_PAGE: 7,
+    BRAND_ID: "58797ed4312455ba35347b5b",
+    KEYWORD: "htc"
+  },
+  MOTOROLA: {
+    FIRST: "motorola-phones-4.php",
+    NEXT: "motorola-phones-f-4-0-p{0}.php",
+    TOTAL_PAGE: 9,
+    BRAND_ID: "58797ed4312455ba35347b5c",
+    KEYWORD: "motorola"
+  },
+  HUAWEI: {
+    FIRST: "huawei-phones-58.php",
+    NEXT: "huawei-phones-f-58-0-p{0}.php",
+    TOTAL_PAGE: 6,
+    BRAND_ID: "58797ed4312455ba35347b5d",
+    KEYWORD: "huawei"
+  },
+  LENOVO: {
+    FIRST: "lenovo-phones-73.php",
+    NEXT: "lenovo-phones-f-73-0-p{0}.php",
+    TOTAL_PAGE: 4,
+    BRAND_ID: "58797ed4312455ba35347b5e",
+    KEYWORD: "lenovo"
+  },
+  XIAOMI: {
+    FIRST: "xiaomi-phones-80.php",
+    NEXT: "xiaomi-phones-f-80-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b5f",
+    KEYWORD: "xiaomi"
+  },
+  GOOGLE: {
+    FIRST: "google-phones-107.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b60",
+    KEYWORD: "google"
+  },
+  ACER: {
+    FIRST: "acer-phones-59.php",
+    NEXT: "acer-phones-f-59-0-p{0}.php",
+    TOTAL_PAGE: 3,
+    BRAND_ID: "58797ed4312455ba35347b61",
+    KEYWORD: "acer"
+  },
+  ASUS: {
+    FIRST: "asus-phones-46.php",
+    NEXT: "asus-phones-f-46-0-p{0}.php",
+    TOTAL_PAGE: 4,
+    BRAND_ID: "58797ed4312455ba35347b62",
+    KEYWORD: "asus"
+  },
+  OPPO: {
+    FIRST: "oppo-phones-82.php",
+    NEXT: "oppo-phones-f-82-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b63",
+    KEYWORD: "oppo"
+  },
+  ONEPLUS: {
+    FIRST: "oneplus-phones-95.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b64",
+    KEYWORD: "oneplus"
+  },
+  MEIZU: {
+    FIRST: "meizu-phones-74.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b65",
+    KEYWORD: "meizu"
+  },
+  BLACKBERRY: {
+    FIRST: "blackberry-phones-36.php",
+    NEXT: "blackberry-phones-f-36-0-p{0}.php",
+    TOTAL_PAGE: 3,
+    BRAND_ID: "58797ed4312455ba35347b66",
+    KEYWORD: "blackberry"
+  },
+  ALCATEL: {
+    FIRST: "alcatel-phones-5.php",
+    NEXT: "alcatel-phones-f-5-0-p{0}.php",
+    TOTAL_PAGE: 9,
+    BRAND_ID: "58797ed4312455ba35347b67",
+    KEYWORD: "alcatel"
+  },
+  ZTE: {
+    FIRST: "zte-phones-62.php",
+    NEXT: "zte-phones-f-62-0-p{0}.php",
+    TOTAL_PAGE: 6,
+    BRAND_ID: "58797ed4312455ba35347b68",
+    KEYWORD: "zte"
+  },
+  TOSHIBA: {
+    FIRST: "toshiba-phones-44.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b69",
+    KEYWORD: "toshiba"
+  },
+  VODAFONE: {
+    FIRST: "vodafone-phones-53.php",
+    NEXT: "vodafone-phones-f-53-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b6a",
+    KEYWORD: "vodafone"
+  },
+  GIGABYTE: {
+    FIRST: "gigabyte-phones-47.php",
+    NEXT: "gigabyte-phones-f-47-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b6b",
+    KEYWORD: "gigabyte"
+  },
+  XOLO: {
+    FIRST: "xolo-phones-85.php",
+    NEXT: "xolo-phones-f-85-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b6c",
+    KEYWORD: "xolo"
+  },
+  LAVA: {
+    FIRST: "lava-phones-94.php",
+    NEXT: "lava-phones-f-94-0-p{0}.php",
+    TOTAL_PAGE: 3,
+    BRAND_ID: "58797ed4312455ba35347b6d",
+    KEYWORD: "lava"
+  },
+  MICROMAX: {
+    FIRST: "micromax-phones-66.php",
+    NEXT: "micromax-phones-f-66-0-p{0}.php",
+    TOTAL_PAGE: 7,
+    BRAND_ID: "58797ed4312455ba35347b6e",
+    KEYWORD: "micromax"
+  },
+  BLU: {
+    FIRST: "blu-phones-67.php",
+    NEXT: "blu-phones-f-67-0-p{0}.php",
+    TOTAL_PAGE: 6,
+    BRAND_ID: "58797ed4312455ba35347b6f",
+    KEYWORD: "blu"
+  },
+  GIONEE: {
+    FIRST: "gionee-phones-92.php",
+    NEXT: "gionee-phones-f-92-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b70",
+    KEYWORD: "gionee"
+  },
+  VIVO: {
+    FIRST: "vivo-phones-98.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b71",
+    KEYWORD: "vivo"
+  },
+  LEECO: {
+    FIRST: "leeco-phones-109.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b72",
+    KEYWORD: "leeco"
+  },
+  PANASONIC: {
+    FIRST: "panasonic-phones-6.php",
+    NEXT: "panasonic-phones-f-6-0-p{0}.php",
+    TOTAL_PAGE: 4,
+    BRAND_ID: "58797ed4312455ba35347b73",
+    KEYWORD: "panasonic"
+  },
+  HP: {
+    FIRST: "hp-phones-41.php",
+    NEXT: "hp-phones-f-41-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b74",
+    KEYWORD: "hp"
+  },
+  YU: {
+    FIRST: "yu-phones-100.php",
+    NEXT: null,
+    TOTAL_PAGE: 1,
+    BRAND_ID: "58797ed4312455ba35347b75",
+    KEYWORD: "yu"
+  },
+  VERYKOOL: {
+    FIRST: "verykool-phones-70.php",
+    NEXT: "verykool-phones-f-70-0-p{0}.php",
+    TOTAL_PAGE: 3,
+    BRAND_ID: "58797ed4312455ba35347b76",
+    KEYWORD: "verykool"
+  },
+  MAXWEST: {
+    FIRST: "maxwest-phones-87.php",
+    NEXT: "maxwest-phones-f-87-0-p{0}.php",
+    TOTAL_PAGE: 2,
+    BRAND_ID: "58797ed4312455ba35347b77",
+    KEYWORD: "maxwest"
+  },
+  PLUM: {
+    FIRST: "plum-phones-72.php",
+    NEXT: "plum-phones-f-72-0-p{0}.php",
+    TOTAL_PAGE: 3,
+    BRAND_ID: "58797ed4312455ba35347b78",
+    KEYWORD: "plum"
+  }
+}
+
+module.exports = urls;
---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var superagent = require('superagent');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var urls = require('./constants');
+var app = express();
+
+app.get('/test', function (req, res) {
+  var brands = Object.keys(urls);
+  for (var brand of brands) {
+    if (urls[brand].NEXT) {
+      console.log(urls[brand].NEXT.replace("{0}", "test"));
+    }
+  }
+  res.send('aman');
+});
+
+function post(payload) {
+  superagent
+    .post('http://52.221.230.61:9000/api/items')
+    .send(payload)
+    .set('Accept', 'application/json')
+    .end(function (err) {
+      if (err) {
+        console.log(`error on saving ${payload.name}`);
+        console.log("Retrying...");
+        post(payload);
+      } else {
+        console.log(`${payload.name} successfully saved`);
+      }
+    });
+}
+
+function requestDom(url, brandId, keyword) {
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+      $('#review-body').filter(function () {
+        var parent = $(".makers");
+        parent.find('ul').children().each(function (idx, element) {
+          var ulElement = $(this);
+          var imgElement = ulElement.find('img');
+          var name = ulElement.find("span").text();
+          var description = imgElement.attr('title');
+          var imageUrl = imgElement.attr('src');
+          var jsonObject = {
+            name: name,
+            description: description,
+            imageurl: imageUrl,
+            brandId: brandId,
+            keyword: keyword
+          }
+          post(jsonObject);
+        });
+      });
+    }
+  });
+}
+
+app.get('/scrape', function (req, res) {
+  var brandNames = Object.keys(urls);
+  for (var brandName of brandNames) {
+    var metaBrand = urls[brandName];
+    var url;
+    for (var i = 1; i <= metaBrand.TOTAL_PAGE; i++) {
+      if (i === 1) {
+        url = `http://www.gsmarena.com/${metaBrand.FIRST}`;
+      } else {
+        var brandUrl = metaBrand.NEXT.replace("{0}", i);
+        url = `http://www.gsmarena.com/${brandUrl}`;
+      }
+      requestDom(url, metaBrand.BRAND_ID, metaBrand.KEYWORD);
+    }
+  }
+  res.send('Check your console!');
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
